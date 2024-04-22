f34e170246ba2ac89afb3d8560b69dab22d51ca0
nicolasnovello <nicolas.novello.16@gmail.com>
2024-04-22 17:02:05 -0300

    incluimos html, css y un poco de js

--- js/main.js
+++ js/main.js
@@ -1,87 +1,29 @@
-alert("Bienvenido al Botinero");
-let nombreUsuario = prompt("Por favor ingrese su nombre");
-while (nombreUsuario === "") {
-  alert("Ingrese un nombre");
-  nombreUsuario = prompt("Por favor ingrese su nombre");
-}
-alert("Hola " + nombreUsuario + ", un gusto verte");
+// cambio de color de la pagina
 
-const productos = [
-  {
-    nombre: "Botín Nike",
-    precio: 5000,
-  },
-  {
-    nombre: "Botín Adidas",
-    precio: 4500,
-  },
-  {
-    nombre: "Botín Puma",
-    precio: 3500,
-  },
-];
+const btnColorDePagina = document.querySelector(".colorDePagina");
 
-const catalogo = () => {
-  let carrito = [];
-  let total = 0;
+btnColorDePagina.addEventListener("click", () => {
+  document.body.classList.toggle("cambioDeColor");
 
-  let mostrarCatalogo = prompt(
-    "¿Estarías interesado en conocer nuestro catálogo?\nResponder con SI o NO"
-  ).toUpperCase();
-
-  while (mostrarCatalogo !== "SI" && mostrarCatalogo !== "NO") {
-    alert("Por favor, selecciona una opción válida (SI o NO).");
-    mostrarCatalogo = prompt(
-      "¿Estarías interesado en conocer nuestro catálogo?\nResponder con SI o NO"
-    ).toUpperCase();
-  }
-
-  if (mostrarCatalogo === "SI") {
-    let opciones = "";
-    while (opciones !== "ESC") {
-      opciones = prompt(
-        "Estas son las opciones que tenemos disponibles en este momento:\n" +
-          productos
-            .map(
-              (producto, index) =>
-                index + 1 + "- " + producto.nombre + ": $" + producto.precio
-            )
-            .join("\n") +
-          "\nColoque el número del botín en el que se encuentra interesado\n" +
-          "Si no es lo que usted esperaba escriba ESC"
-      ).toUpperCase();
-
-      if (!isNaN(opciones)) {
-        opciones = parseInt(opciones);
-        if (opciones >= 1 && opciones <= productos.length) {
-          let productoSeleccionado = productos[opciones - 1];
-          carrito.push(productoSeleccionado);
-          total += productoSeleccionado.precio;
-          let continuarComprando = confirm(
-            `Ha seleccionado: ${productoSeleccionado.nombre}\nPrecio: $${productoSeleccionado.precio}\n¿Desea seguir comprando?`
-          );
-
-          if (!continuarComprando) {
-            alert(
-              `¡Gracias por su compra, ${nombreUsuario}! Monto total: $${total}`
-            );
-            break;
-          }
-        }
-      } else if (opciones === "ESC") {
-        if (carrito.length > 0) {
-          alert(
-            `¡Gracias por su compra, ${nombreUsuario}! Monto total: $${total}`
-          );
-        } else {
-          alert("¡Hasta luego!");
-        }
-        break;
-      }
-    }
+  if (document.body.classList.contains("cambioDeColor")) {
+    btnColorDePagina.innerText = "Modo claro";
+    localStorage.setItem("modoColor", "oscuro");
   } else {
-    alert("¡Hasta luego!");
+    btnColorDePagina.innerText = "Modo oscuro";
+    localStorage.setItem("modoColor", "claro");
   }
-};
+});
 
-catalogo();
+document.addEventListener("DOMContentLoaded", () => {
+  const modoColor = localStorage.getItem("modoColor");
+
+  if (modoColor === "oscuro") {
+    document.body.classList.add("cambioDeColor");
+    document.querySelector(".colorDePagina").innerText = "Modo claro";
+  } else {
+    document.body.classList.remove("cambioDeColor");
+    document.querySelector(".colorDePagina").innerText = "Modo oscuro";
+  }
+});
+
+//Simulacion de inicio de sesion
