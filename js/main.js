c717abd33e3bd4c527af2b44d955448b02f4122b
nicolasnovello <nicolas.novello.16@gmail.com>
2024-04-23 20:32:11 -0300

    ultimo commit tratando de solucionar errores

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ botines.forEach((producto) => {
 function carritoActualizado() {
   if (carrito.length === 0) {
     carritoVacio.classList.remove("ocultar");
-    productosDelCarrito.classList.class("ocultar");
+    productosDelCarrito.classList.remove("ocultar");
   } else {
     carritoVacio.classList.add("ocultar");
     productosDelCarrito.classList.remove("ocultar");
@@ -115,6 +115,7 @@ const eliminarProducto = (titulo) => {
   carritoActualizado();
   mostrarTotalCompra();
 };
+carritoActualizado();
 
 // Event listener para el boton eliminar
 document.addEventListener("click", (event) => {
@@ -133,65 +134,14 @@ const mostrarTotalCompra = () => {
 };
 
 mostrarTotalCompra();
-carritoActualizado();
 
 //Cambio de color de la pagina
-
-const btnColorDePagina = document.querySelector(".colorDePagina");
-
-btnColorDePagina.addEventListener("click", () => {
+const botonCambioColor = document.querySelector(".color");
+botonCambioColor.addEventListener("click", () => {
   document.body.classList.toggle("cambioDeColor");
-
   if (document.body.classList.contains("cambioDeColor")) {
-    btnColorDePagina.innerText = "Modo claro";
-    localStorage.setItem("modoColor", "oscuro");
+    botonCambioColor.innerText = "Modo claro";
   } else {
-    btnColorDePagina.innerText = "Modo oscuro";
-    localStorage.setItem("modoColor", "claro");
+    botonCambioColor.innerText = "Modo oscuro";
   }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const modoColor = localStorage.getItem("modoColor");
-
-  if (modoColor === "oscuro") {
-    document.body.classList.add("cambioDeColor");
-    document.querySelector(".colorDePagina").innerText = "Modo claro";
-  } else {
-    document.body.classList.remove("cambioDeColor");
-    document.querySelector(".colorDePagina").innerText = "Modo oscuro";
-  }
-});
-
-//Simulacion de inicio de sesion
-document.addEventListener("DOMContentLoaded", function () {
-  const registroDeFormulario = document.getElementById("loginForm");
-  const entradaDeNombre = document.getElementById("username");
-  const enlaceDeUsuario = document.querySelector(
-    ".barraNavegacion ul li:nth-child(2) a"
-  );
-
-  const usuarioRegistrado = localStorage.getItem("usuarioRegistrado");
-  if (usuarioRegistrado) {
-    enlaceDeUsuario.textContent = "Hola, " + usuarioRegistrado;
-  }
-
-  registroDeFormulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const nombreDeUsuario = entradaDeNombre.value.trim();
-
-    if (!nombreDeUsuario) {
-      alert("Por favor, ingrese un nombre de usuario.");
-      return;
-    }
-
-    localStorage.setItem("usuarioRegistrado", nombreDeUsuario);
-
-    enlaceDeUsuario.textContent = "Hola, " + nombreDeUsuario;
-
-    alert("Inicio de sesión exitoso. ¡Hola, " + nombreDeUsuario + "!");
-
-    registroDeFormulario.reset();
-  });
 });
